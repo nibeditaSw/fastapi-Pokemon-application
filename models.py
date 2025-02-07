424a6f428576179d1201cdb4d6a1bb9bbef04e68
nibedita.swain <nibedita.swain@annalect.com>
2025-02-07 13:52:47 +0530

    updated db migrations with alembic

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 class Pokemon(Base):
-    __tablename__ = "pokemon"
+    __tablename__ = "pokemons"
     
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
@@ -14,16 +14,16 @@ class Pokemon(Base):
     pokemon_url = Column(String)
 
     # Relationships
-    abilities = relationship("Ability", back_populates="pokemon", cascade="all, delete")
-    stats = relationship("Stat", back_populates="pokemon", cascade="all, delete")
-    types = relationship("Type", back_populates="pokemon", cascade="all, delete")
+    abilities = relationship("Ability", back_populates="pokemons", cascade="all, delete")
+    stats = relationship("Stat", back_populates="pokemons", cascade="all, delete")
+    types = relationship("Type", back_populates="pokemons", cascade="all, delete")
 
 
 class Ability(Base):
     __tablename__ = "abilities"
     
     id = Column(Integer, primary_key=True, index=True)
-    pokemon_id = Column(Integer, ForeignKey("pokemon.id"))
+    pokemon_id = Column(Integer, ForeignKey("pokemons.id"))
     name = Column(String, nullable=False)
     is_hidden = Column(Boolean, default=False)
 
@@ -34,7 +34,7 @@ class Stat(Base):
     __tablename__ = "stats"
     
     id = Column(Integer, primary_key=True, index=True)
-    pokemon_id = Column(Integer, ForeignKey("pokemon.id"))
+    pokemon_id = Column(Integer, ForeignKey("pokemons.id"))
     name = Column(String, nullable=False)
     base_stat = Column(Integer)
 
@@ -45,7 +45,7 @@ class Type(Base):
     __tablename__ = "types"
     
     id = Column(Integer, primary_key=True, index=True)
-    pokemon_id = Column(Integer, ForeignKey("pokemon.id"))
+    pokemon_id = Column(Integer, ForeignKey("pokemons.id"))
     name = Column(String, nullable=False)
 
     pokemon = relationship("Pokemon", back_populates="types")
